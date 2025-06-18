17a9da394c74a7a8183284ca07c55786d8f9b7e1
sbhor007 <sbhor747@gmail.com>
2025-06-18 17:28:13 +0530

    .

--- Backend/Apps/Photos/views.py
+++ Backend/Apps/Photos/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from .serializers import CategorySerializer,TagsSerializer,PhotoSerializer, SavePhotosSerializer
 from .models import Tags,Category,Photo,SavePhotos
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
+from rest_framework import permissions,status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .permission import IsOwnerOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 # Create your views here.
 
@@ -45,11 +48,26 @@ class PhotoViewSet(ModelViewSet):
 class SavePhotosViewSet(ModelViewSet):
     queryset = SavePhotos.objects.all()
     serializer_class = SavePhotosSerializer
-    permission_classes = (permissions.IsAuthenticated,IsOwnerOrReadOnly)
+    permission_classes = (permissions.IsAuthenticated,)
     authentication_classes = (JWTAuthentication,) 
     
     def get_queryset(self):
-        user = self.request.user
-        queryset = SavePhotos.objects.filter(user=user)
-        return queryset
+        # user = self.request.user
+        # queryset = SavePhotos.objects.filter(user=user)
+        # return queryset
+        return SavePhotos.objects.all()
+    
+    @action(detail=False,methods=['get'],url_path='count')
+    def save_photo_count(self,request,*args, **kwargs):
+        photo_id = request.query_params.get('photoId')
+        if not photo_id:
+            return Response(
+                {
+                    "message": "Missing 'photo' ID."
+                    },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        count = self.get_queryset().filter(photo=photo_id).count()
+        print('*'*50,count,'*'*50)
+        return Response(count,status=status.HTTP_200_OK)
     
